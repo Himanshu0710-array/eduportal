8c9817a08ba8ddb714de02f1a3b105576fa9533b
Himanshu0710-array <himanshuchandlani07@gmail.com>
2026-05-17 02:08:33 +0530

    Grant 127.0.0.1 root permissions in MariaDB

--- start.sh
+++ start.sh
@@ -5,6 +5,11 @@
 # Wait for MariaDB to start
 sleep 3
 
+# Configure MariaDB user permissions so PHP (www-data) can connect
+mysql -u root -e "CREATE USER IF NOT EXISTS 'root'@'127.0.0.1' IDENTIFIED BY '';"
+mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'127.0.0.1' WITH GRANT OPTION;"
+mysql -u root -e "FLUSH PRIVILEGES;"
+
 # Import the database schema
 mysql -u root < /var/www/html/database.sql
 
