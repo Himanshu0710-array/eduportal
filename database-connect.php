285ff8df72d1ec0c283821b73693b09cea1569f0
Himanshu0710-array <himanshuchandlani07@gmail.com>
2026-05-21 13:13:31 +0530

    Fix: Added SSL options to PDO connection for TiDB Cloud

--- database-connect.php
+++ database-connect.php
@@ -11,13 +11,19 @@ if (PHP_OS_FAMILY === 'Windows') {
 $dbname     = getenv("DB_NAME") ?: "himanshu_4604"; 
 
 try {
-   
-    $conn = new PDO("mysql:host=$servername;dbname=$dbname;charset=utf8", $username, $password);
-
+    $options = array(
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+    );
     
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    // TiDB requires SSL. Use Render's default certificate path on Linux.
+    if (PHP_OS_FAMILY !== 'Windows') {
+        $options[PDO::MYSQL_ATTR_SSL_CA] = '/etc/ssl/certs/ca-certificates.crt';
+    } else if (strpos($servername, 'tidbcloud') !== false) {
+        // If testing TiDB locally on Windows XAMPP, disable verification to bypass missing CA
+        $options[PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT] = false;
+    }
 
-    
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname;charset=utf8", $username, $password, $options);
 
 } catch (PDOException $e) {
  
